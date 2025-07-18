e780eb1fdec3c149bbeddac45d51dcc483829f04
meepodeep <nbltmepodep@gmail.com>
2025-07-18 00:08:12 -0400

    worked on projects pg

--- personal-site/src/routes/projects/+page.svelte
+++ personal-site/src/routes/projects/+page.svelte
@@ -2,5 +2,16 @@
     let img = "/images/ProjectsBG.png"
 </script>
 <main class="w-screen h-screen overflow-hidden">
+    
     <img src= { img } alt="Example" class="w-full h-full object-cover"/>
+    <div class="no-scrollbar w-full overflow-x-auto whitespace-nowrap p-4 absolute top-7/10 left-1/2 transform -translate-x-1/2 -translate-y-2/5">
+    <div class="inline-flex gap-4">
+        <img src="/images/Square filler.png" class="h-48 rounded-lg shadow-md" alt="Image 1" />
+        <img src="/images/Square filler.png" class="h-48 rounded-lg shadow-md" alt="Image 2" />
+        <img src="/images/Square filler.png" class="h-48 rounded-lg shadow-md" alt="Image 3" />
+        <img src="/images/Square filler.png" class="h-48 rounded-lg shadow-md" alt="Image 4" />
+        <img src="/images/Square filler.png" class="h-48 rounded-lg shadow-md" alt="Image 5" />
+        <!-- Add more as needed -->
+    </div>
+</div>
 </main>
