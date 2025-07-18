cb1615a38dc14320008de4090412bf8ee1ad9014
meepodeep <nbltmepodep@gmail.com>
2025-07-18 01:38:47 -0400

    worked on the new projects page system
    inspired by the hack mac hc sticker!!!

--- personal-site/src/routes/projects/+page.svelte
+++ personal-site/src/routes/projects/+page.svelte
@@ -4,7 +4,7 @@
 <main class="w-screen h-screen overflow-hidden">
     
     <img src= { img } alt="Example" class="w-full h-full object-cover"/>
-    <div class="no-scrollbar w-full overflow-x-auto whitespace-nowrap p-4 absolute top-7/10 left-1/2 transform -translate-x-1/2 -translate-y-2/5">
+    <div class="no-scrollbar overflow-x-auto whitespace-nowrap p-4 absolute top-7/10 left-1/2 transform -translate-x-1/2 -translate-y-2/5 w-70">
     <div class="inline-flex gap-4">
         <img src="/images/Square filler.png" class="h-48 rounded-lg shadow-md" alt="Image 1" />
         <img src="/images/Square filler.png" class="h-48 rounded-lg shadow-md" alt="Image 2" />
