3dceb5ad360c5ec64fbfff19109236376c0c57dd
meepodeep <nbltmepodep@gmail.com>
2025-07-18 22:38:16 -0400

    fixed scaling to mobile of the projects pg

--- personal-site/src/routes/projects/+page.svelte
+++ personal-site/src/routes/projects/+page.svelte
@@ -8,23 +8,24 @@
         background-color: theme(--color-amber-100); 
     }
 </style>
-<main class="w-screen h-screen overflow-hidden font-128k-mac text-amber-900">
-    <img src= { img } alt="Example" class="w-full h-full object-cover"/>
+<main class="overflow-hidden font-128k-mac text-black">
+    <img src= { img } alt="Example" class="w-screen h-screen object-cover"/>
     <!-- main scroll box-->
-    <div class="no-scrollbar h-102 w-140 overflow-y-scroll absolute top-1/2 left-4/8 transform -translate-x-7/16 -translate-y-6/8">
+    <div class="bg-amber-50 font-osx overscroll-contain no-scrollbar h-1/2 w-75 md:w-4/12 overflow-y-scroll absolute top-1/2 left-4/8 transform -translate-x-29/64 -translate-y-6/8">
     <div class="content grid grid-cols-2 p-4 space-y-4">
          <video class="w-75 h-75 overflow-hidden rounded-2xl" controls>
+            <track kind="captions">
             <source src="/images/Drone Flight.mp4" type="video/mp4">
             Your browser does not support the video tag.
         </video> 
         <div>    
-            <h1 class="px-2 font-pacifico text-3xl">
+            <h1 class="px-2 text-l md:text-3xl">
                 Delivery Drone
             </h1>   
-            <h2 class="font-pacifico text-xl">
+            <h2 class="px-2 text-sm md:text-xl">
                 Features
             </h2>
-            <ul class="list-disc px-6 text-l">
+            <ul class="list-disc px-6 text-xs md:text-l">
                 <li>2x 2200mah 3s lipos</li>
                 <li>Full autonomy using ardupilot</li>
                 <li>7km range</li>
@@ -33,10 +34,10 @@
             </ul>   
 
             <div class="px-2">
-                <h2 class="font-pacifico text-xl">
+                <h2 class="text-sm md:text-xl">
                     GitHub
                 </h2>
-                <h3 class="px-4 text-l">
+                <h3 class="px-4 text-xs md:text-l">
                     Not yet!
                 </h3>
             </div>
