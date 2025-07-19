eed550eb45f1d581118cb6d2f0bbea4e0a160440
meepodeep <nbltmepodep@gmail.com>
2025-07-19 13:13:22 -0400

    added more buttons to the main page

--- personal-site/src/routes/+page.svelte
+++ personal-site/src/routes/+page.svelte
@@ -33,6 +33,7 @@
 
 <main class="w-screen h-screen overflow-hidden">
     <img src= { img } alt="Example" class="w-full h-full object-cover"/>
+<!--main proj button-->
 <button
         on:click={projects}
         on:mouseenter={onHoverProjects}
@@ -41,6 +42,16 @@
         >
         projects
     </button>
+<!--Hack button-->
+<button
+        on:click={projects}
+        on:mouseenter={onHoverProjects}
+        on:mouseleave={onLeave}
+        class = "bg-amber-50 absolute top-1/2 left-4/6 transform -translate-x-2/6 -translate-y-3/5 w-40 h-45 opacity-0"
+        >
+        projects
+    </button>
+<!--About button-->
 <button
 
         on:mouseenter={onHoverAbout}
@@ -49,11 +60,30 @@
         >
         About
     </button>
+<!--Machine button-->
+<button
+
+        on:mouseenter={onHoverAbout}
+        on:mouseleave={onLeave}
+        class = "bg-amber-50 absolute top-4/9 left-1/4 transform -translate-x-1/2 -translate-y-1/4 w-80 h-75 opacity-0"
+        >
+        About
+    </button>
+<!--links button-->
 <button
 
         on:mouseenter={onHoverLinks}
         on:mouseleave={onLeave}
         class = "bg-amber-50 absolute top-6/11 left-6/11 transform -translate-x-5/10 -translate-y-4/5 w-35 h-10 opacity-0"
+        >
+        Links
+    </button>
+<!--links object button-->
+<button
+
+        on:mouseenter={onHoverLinks}
+        on:mouseleave={onLeave}
+        class = "bg-amber-50 absolute top-1/2 left-0/2 transform -translate-x-1/5 -translate-y-1/2 w-55 h-75 opacity-0"
         >
         Links
     </button>
--- personal-site/src/routes/projects/+page.svelte
+++ personal-site/src/routes/projects/+page.svelte
@@ -13,11 +13,13 @@
     <!-- main scroll box-->
     <div class=" font-osx overscroll-contain no-scrollbar h-80 md:h-85 w-100 md:w-130 overflow-y-scroll absolute top-1/2 left-4/8 transform -translate-x-29/64 -translate-y-13/19 md:-translate-y-15/19">
     <div class="content grid grid-cols-2 p-4 space-y-4">
+        <!--Delivery Drone Vid-->
          <video class="w-75 h-75 overflow-hidden rounded-2xl" controls>
             <track kind="captions">
             <source src="/images/Drone Flight.mp4" type="video/mp4">
             Your browser does not support the video tag.
         </video> 
+        <!--Delivery Drone Text-->
         <div>    
             <h1 class="px-2 text-l md:text-3xl">
                 Delivery Drone
@@ -30,7 +32,7 @@
                 <li>Full autonomy using ardupilot</li>
                 <li>7km range</li>
                 <li>no longer working :(</li>
-                <li>3d printed or foam frame</li>
+                <li>3d printed or foam fuselage and wings</li>
             </ul>   
 
             <div class="px-2">
@@ -41,11 +43,31 @@
                     Not yet!
                 </h3>
             </div>
-
         </div>   
-        <img src="/images/Square filler.png" class="rounded-lg shadow-md shrink" alt="Image 1" />     
-        <div class="item-body px-6">        
-            First project
+        <img src="/images/Modular cyberdeck.webp" class="rounded-lg shadow-md shrink" alt="Image 1" />     
+        <div>    
+            <h1 class="px-2 text-l md:text-3xl">
+                ModuDeck
+            </h1>   
+            <h2 class="px-2 text-sm md:text-xl">
+                Features
+            </h2>
+            <ul class="list-disc px-6 text-xs md:text-l">
+                <li>4s lion hot swappable battery. 8 hr max battery life!</li>
+                <li>Support for Lattepanda mu, pi 5, and pi zero sbc modules.</li>
+                <li>Module templates to design your own!</li>
+                <li>Ortholinear mechanical keyboard.</li>
+                <li>Ultrawide display.</li>
+            </ul>   
+
+            <div class="px-2">
+                <h2 class="text-sm md:text-xl">
+                    GitHub
+                </h2>
+                <h3 class="px-4 text-xs md:text-l text-blue-800">
+                    <a href="https://github.com/meepodeep/modudeck">Here!</a>
+                </h3>
+            </div>
         </div>   
         <img src="/images/Square filler.png" class="rounded-lg shadow-md shrink" alt="Image 1" />     
         <div class="item-body px-6">        
