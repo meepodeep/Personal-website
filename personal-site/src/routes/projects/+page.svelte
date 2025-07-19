561c524ef9169d2abdc9c03999b23d723e4d1300
meepodeep <nbltmepodep@gmail.com>
2025-07-19 19:20:24 -0400

    worked on more mobile scaling 😭😭😭😭

--- personal-site/src/routes/projects/+page.svelte
+++ personal-site/src/routes/projects/+page.svelte
@@ -11,7 +11,7 @@
 <main class="overflow-hidden font-128k-mac text-black">
     <img src= { img } alt="Example" class="w-screen h-screen object-cover"/>
     <!-- main scroll box-->
-    <div class="font-osx overscroll-contain no-scrollbar h-80 md:h-70 lg:h-80 w-85 md:w-120 lg:w-140 overflow-y-scroll absolute top-1/2 left-4/8 transform -translate-x-32/64 md:-translate-x-29/64 lg:-translate-x-29/64 -translate-y-15/19">
+    <div class="font-osx overscroll-contain no-scrollbar h-80 md:h-100 lg:h-80 w-85 md:w-120 lg:w-140 overflow-y-scroll absolute top-1/2 left-4/8 transform -translate-x-32/64 md:-translate-x-29/64 lg:-translate-x-29/64 -translate-y-15/19">
     <div class="content grid grid-cols-2 p-4 space-y-4">
         <!--Delivery Drone Vid-->
          <video class="pt-4 w-75 h-75 overflow-hidden rounded-2xl" controls>
@@ -58,11 +58,10 @@
             <h2 class="pl-2 text-sm md:text-xl lg:text-2xl">
                 Features
             </h2>
-            <ul class="list-disc px-6 text-tiny md:text-sm lg:text-sm 2xl:text-xl">
+            <ul class="list-disc px-6 text-tiny md:text-sm lg:text-l 2xl:text-xl">
                 <li>Modular batteries and compute modules!</li>
                 <li>Open source modules!</li>
                 <li>Ortholinear keyboard!</li>
-                <li>Ultrawide display!</li>
             </ul>   
 
             <div class="px-2">
@@ -98,32 +97,32 @@
                     <a href="https://github.com/meepodeep/ergoDecks">Here!</a>
                 </h3>
             </div>
-        </div>  
-        <img src="/images/Modular cyberdeck.webp" class="rounded-lg shadow-md shrink" alt="Image 1" />    
+        </div>
+        <img src="/images/pedals.webp" class="rounded-lg shadow-md shrink" alt="Image 1" />    
         <div>    
             <h1 class="px-2 text-l md:text-xl lg:text-3xl">
-                ModuDeck
+                Pedals
             </h1>   
+            <h3 class="px-2 pt-2 text-tiny md:text-sm lg:text-l 2xl:text-xl">
+                Flight sim pedals
+            </h3>
             <h2 class="pl-2 text-sm md:text-xl lg:text-2xl">
                 Features
             </h2>
             <ul class="list-disc px-6 text-tiny md:text-sm lg:text-l 2xl:text-xl">
-                <li>4s lion hot swappable battery. 8 hr max battery life!</li>
-                <li>Support for Lattepanda mu, pi 5, and pi zero sbc modules.</li>
-                <li>Open source modules!</li>
-                <li>Ortholinear mechanical keyboard.</li>
-                <li>Ultrawide display.</li>
+                <li>Reads as a gamepad joystick</li>
+                <li>Works with any game</li>
+                <li>90% 3d printed</li>
             </ul>   
-
             <div class="px-2">
                 <h2 class="text-sm md:text-xl lg:text-2xl">
                     GitHub
                 </h2>
                 <h3 class="px-4 text-tiny md:text-sm lg:text-l 2xl:text-xl text-blue-800">
-                    <a href="https://github.com/meepodeep/modudeck">Here!</a>
+                    <a href="">not yet</a>
                 </h3>
             </div>
-        </div>   
+        </div>
         <!-- Add more as needed -->
     </div>
 </div>
